a9bf01afde64e957415f37f43f739fc3f992f6ce
Olia-dolia <dolechka4@gmail.com>
2023-11-03 20:38:47 +0200

    ProxyCredentials equals/hashcode test

--- executor-service/src/test/java/executor/service/model/ProxyCredentialsTest.java
+++ executor-service/src/test/java/executor/service/model/ProxyCredentialsTest.java
@@ -1,7 +1,6 @@
 package executor.service.model;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -38,5 +37,33 @@ public class ProxyCredentialsTest {
     void setPassword() {
         credentials2.setPassword("newPassword");
         assertEquals("newPassword", credentials2.getPassword());
+    }
+
+    @Test
+    void testEquals() {
+        ProxyCredentials saveCredentials =
+                new ProxyCredentials("username1", "password");
+        assertEquals(credentials1, saveCredentials);
+    }
+
+    @Test
+    void testEqualsDifferentCredentials(){
+        assertNotEquals(credentials1, credentials2);
+    }
+
+    @Test
+    void testHashCode() {
+        int expectedHashCode = credentials1.hashCode();
+        assertEquals(expectedHashCode, credentials1.hashCode());
+    }
+
+    @Test
+    void testEqualsAndHashCode() {
+        ProxyCredentials credentials2 = new ProxyCredentials();
+        credentials2.setUsername("username1");
+        credentials2.setPassword("password");
+
+        assertEquals(credentials1, credentials2);
+        assertEquals(credentials1.hashCode(), credentials2.hashCode());
     }
 }
