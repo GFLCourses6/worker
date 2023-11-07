ddfb93daf8e0bf23dc01f3f0f08846292865e3dd
MaxT07MT <135219328+MaxT07MT@users.noreply.github.com>
2023-11-07 18:47:35 +0200

    WebDriverInitializerImpl fix2

--- executor-service/src/main/java/executor/service/service/webDriver/impl/WebDriverInitializerImpl.java
+++ executor-service/src/main/java/executor/service/service/webDriver/impl/WebDriverInitializerImpl.java
@@ -1,30 +1,81 @@
 package executor.service.service.webDriver.impl;
 
+import static java.time.Duration.ofSeconds;
+
 import executor.service.model.ProxyConfigHolder;
+import executor.service.model.ProxyNetworkConfig;
+import executor.service.model.ProxyCredentials;
 import executor.service.model.WebDriverConfig;
+import executor.service.service.ConfigPropertiesLoader;
 import executor.service.service.webDriver.WebDriverInitializer;
-import java.time.Duration;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class WebDriverInitializerImpl implements WebDriverInitializer {
-  private final WebDriverConfig webDriverConfig;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Properties;
 
-  public WebDriverInitializerImpl(WebDriverConfig webDriverConfig, ProxyConfigHolder proxyConfigHolder) {
+public class WebDriverInitializerImpl implements WebDriverInitializer {
+
+  private final WebDriverConfig webDriverConfig;
+  private ProxyConfigHolder proxyConfigHolder;
+
+  public WebDriverInitializerImpl(WebDriverConfig webDriverConfig,
+      ProxyConfigHolder proxyConfigHolder) {
     this.webDriverConfig = webDriverConfig;
+    this.proxyConfigHolder = proxyConfigHolder;
+    loadConfigProperties();
+  }
+
+  private void loadConfigProperties() {
+    Properties properties = ConfigPropertiesLoader.loadConfigProperties("config.properties");
+    webDriverConfig.setUserAgent(properties.getProperty("userAgent"));
+    webDriverConfig.setPageLoadTimeout(Long.parseLong(properties.getProperty("pageLoadTimeout")));
+    webDriverConfig.setImplicitlyWait(Long.parseLong(properties.getProperty("implicitlyWait")));
+
+    ProxyNetworkConfig proxyNetworkConfig = new ProxyNetworkConfig(
+        properties.getProperty("proxyHostname"),
+        Integer.parseInt(properties.getProperty("proxyPort"))
+    );
+    ProxyCredentials proxyCredentials = new ProxyCredentials(
+        properties.getProperty("proxyUsername"),
+        properties.getProperty("proxyPassword")
+    );
+    proxyConfigHolder = new ProxyConfigHolder(proxyNetworkConfig, proxyCredentials);
+  }
+
+  public ChromeOptions getChromeOptions() {
+    ChromeOptions chromeOptions = new ChromeOptions();
+    chromeOptions.addArguments("--user-agent=" + webDriverConfig.getUserAgent());
+    chromeOptions.addArguments(generateProxyArguments(proxyConfigHolder));
+    return chromeOptions;
+  }
+
+  public List<String> generateProxyArguments(ProxyConfigHolder proxyConfigHolder) {
+    List<String> arguments = new ArrayList<>();
+    ProxyNetworkConfig proxyNetworkConfig = proxyConfigHolder.getProxyNetworkConfig();
+    if (proxyNetworkConfig != null) {
+      arguments.add("--proxy-hostname=" + proxyNetworkConfig.getHostname());
+      arguments.add("--proxy-port=" + proxyNetworkConfig.getPort());
+    }
+
+    ProxyCredentials proxyCredentials = proxyConfigHolder.getProxyCredentials();
+    if (proxyCredentials != null) {
+      arguments.add("--proxy-username=" + proxyCredentials.getUsername());
+      arguments.add("--proxy-password=" + proxyCredentials.getPassword());
+    }
+
+    return arguments;
   }
 
   @Override
   public WebDriver create() {
-    System.setProperty("config.properties", webDriverConfig.getWebDriverExecutable());
-    ChromeOptions chromeOptions = new ChromeOptions();
-    chromeOptions.addArguments("--user-agent=" + webDriverConfig.getUserAgent());
-    WebDriver driver = new ChromeDriver(chromeOptions);
-    Duration pageLoadTimeout = Duration.ofSeconds(webDriverConfig.getPageLoadTimeout());
-    driver.manage().timeouts().pageLoadTimeout(pageLoadTimeout);
-    Duration implicitlyWait = Duration.ofSeconds(webDriverConfig.getImplicitlyWait());
-    driver.manage().timeouts().implicitlyWait(implicitlyWait);
+    WebDriverManager.chromedriver().setup();
+    WebDriver driver = new ChromeDriver(getChromeOptions());
+    driver.manage().timeouts().pageLoadTimeout(ofSeconds(webDriverConfig.getPageLoadTimeout()));
+    driver.manage().timeouts().implicitlyWait(ofSeconds(webDriverConfig.getImplicitlyWait()));
     return driver;
   }
 }
