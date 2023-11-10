b527cef6b5ae66f47ba118c5d5bd058e0e2ad37e
Mazurenko Nikita <nikitamazurenko42@gmail.com>
2023-11-10 12:20:07 +0200

    Configure proxy server & proxy server authentication in the WebDriverInitializerImpl

--- executor-service/src/main/java/executor/service/service/ConfigPropertiesLoader.java
+++ executor-service/src/main/java/executor/service/service/ConfigPropertiesLoader.java
@@ -1,17 +1,18 @@
 package executor.service.service;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class ConfigPropertiesLoader {
-  public static Properties loadConfigProperties(String filename) {
-    Properties properties = new Properties();
-    try (InputStream input = ConfigPropertiesLoader.class.getClassLoader().getResourceAsStream(filename)) {
-      properties.load(input);
-    } catch (IOException e) {
-      e.printStackTrace();
-      throw new RuntimeException("Error reading settings from " + filename + " file");
+
+    public Properties loadConfigProperties(String filename) {
+        Properties properties = new Properties();
+        try (InputStream input = ConfigPropertiesLoader.class.getClassLoader().getResourceAsStream(filename)) {
+            properties.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading settings from " + filename + " file");
+        }
+        return properties;
     }
-    return properties;
-  }
 }
--- executor-service/src/main/java/executor/service/service/webDriver/impl/WebDriverInitializerImpl.java
+++ executor-service/src/main/java/executor/service/service/webDriver/impl/WebDriverInitializerImpl.java
@@ -1,81 +1,93 @@
 package executor.service.service.webDriver.impl;
 
-import static java.time.Duration.ofSeconds;
-
 import executor.service.model.ProxyConfigHolder;
-import executor.service.model.ProxyNetworkConfig;
 import executor.service.model.ProxyCredentials;
+import executor.service.model.ProxyNetworkConfig;
 import executor.service.model.WebDriverConfig;
 import executor.service.service.ConfigPropertiesLoader;
 import executor.service.service.webDriver.WebDriverInitializer;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.io.File;
 import java.util.Properties;
+
+import static java.time.Duration.ofSeconds;
 
 public class WebDriverInitializerImpl implements WebDriverInitializer {
 
-  private final WebDriverConfig webDriverConfig;
-  private ProxyConfigHolder proxyConfigHolder;
+    private final ConfigPropertiesLoader configPropertiesLoader;
 
-  public WebDriverInitializerImpl(WebDriverConfig webDriverConfig,
-      ProxyConfigHolder proxyConfigHolder) {
-    this.webDriverConfig = webDriverConfig;
-    this.proxyConfigHolder = proxyConfigHolder;
-    loadConfigProperties();
-  }
-
-  private void loadConfigProperties() {
-    Properties properties = ConfigPropertiesLoader.loadConfigProperties("config.properties");
-    webDriverConfig.setUserAgent(properties.getProperty("userAgent"));
-    webDriverConfig.setPageLoadTimeout(Long.parseLong(properties.getProperty("pageLoadTimeout")));
-    webDriverConfig.setImplicitlyWait(Long.parseLong(properties.getProperty("implicitlyWait")));
-
-    ProxyNetworkConfig proxyNetworkConfig = new ProxyNetworkConfig(
-        properties.getProperty("proxyHostname"),
-        Integer.parseInt(properties.getProperty("proxyPort"))
-    );
-    ProxyCredentials proxyCredentials = new ProxyCredentials(
-        properties.getProperty("proxyUsername"),
-        properties.getProperty("proxyPassword")
-    );
-    proxyConfigHolder = new ProxyConfigHolder(proxyNetworkConfig, proxyCredentials);
-  }
-
-  public ChromeOptions getChromeOptions() {
-    ChromeOptions chromeOptions = new ChromeOptions();
-    chromeOptions.addArguments("--user-agent=" + webDriverConfig.getUserAgent());
-    chromeOptions.addArguments(generateProxyArguments(proxyConfigHolder));
-    return chromeOptions;
-  }
-
-  public List<String> generateProxyArguments(ProxyConfigHolder proxyConfigHolder) {
-    List<String> arguments = new ArrayList<>();
-    ProxyNetworkConfig proxyNetworkConfig = proxyConfigHolder.getProxyNetworkConfig();
-    if (proxyNetworkConfig != null) {
-      arguments.add("--proxy-hostname=" + proxyNetworkConfig.getHostname());
-      arguments.add("--proxy-port=" + proxyNetworkConfig.getPort());
+    public WebDriverInitializerImpl(ConfigPropertiesLoader configPropertiesLoader) {
+        this.configPropertiesLoader = configPropertiesLoader;
     }
 
-    ProxyCredentials proxyCredentials = proxyConfigHolder.getProxyCredentials();
-    if (proxyCredentials != null) {
-      arguments.add("--proxy-username=" + proxyCredentials.getUsername());
-      arguments.add("--proxy-password=" + proxyCredentials.getPassword());
+    @Override
+    public WebDriver create(ProxyConfigHolder proxyConfigHolder) {
+        WebDriverManager.chromedriver().setup();
+
+        WebDriverConfig webDriverConfig = new WebDriverConfig(); // todo: make a component
+        loadWebDriverConfigProperties(webDriverConfig);
+
+        ChromeOptions chromeOptions = getChromeOptions(webDriverConfig);
+        configureProxy(chromeOptions, proxyConfigHolder);
+
+        ChromeDriver driver = new ChromeDriver(chromeOptions);
+        configureProxyAuth(driver, proxyConfigHolder);
+
+        driver.manage().timeouts().pageLoadTimeout(ofSeconds(webDriverConfig.getPageLoadTimeout()));
+        driver.manage().timeouts().implicitlyWait(ofSeconds(webDriverConfig.getImplicitlyWait()));
+
+        return driver;
     }
 
-    return arguments;
-  }
+    private ChromeOptions getChromeOptions(WebDriverConfig webDriverConfig) {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--user-agent=" + webDriverConfig.getUserAgent());
+        chromeOptions.setAcceptInsecureCerts(true);
 
-  @Override
-  public WebDriver create() {
-    WebDriverManager.chromedriver().setup();
-    WebDriver driver = new ChromeDriver(getChromeOptions());
-    driver.manage().timeouts().pageLoadTimeout(ofSeconds(webDriverConfig.getPageLoadTimeout()));
-    driver.manage().timeouts().implicitlyWait(ofSeconds(webDriverConfig.getImplicitlyWait()));
-    return driver;
-  }
+        return chromeOptions;
+    }
+
+    private void configureProxy(ChromeOptions chromeOptions, ProxyConfigHolder proxyConfigHolder) {
+        ProxyNetworkConfig networkConfig = proxyConfigHolder.getProxyNetworkConfig();
+        ProxyCredentials proxyCredentials = proxyConfigHolder.getProxyCredentials();
+
+        if (networkConfig != null) {
+            String hostname = networkConfig.getHostname();
+            Integer port = networkConfig.getPort();
+            chromeOptions.addArguments(String.format("--proxy-server=%s:%d", hostname, port));
+        }
+        if (networkConfig != null && proxyCredentials != null) {
+            chromeOptions.addExtensions(new File("src/main/resources/MultiPass-for-HTTP-basic-authentication.crx"));
+        }
+    }
+
+    private void configureProxyAuth(WebDriver driver, ProxyConfigHolder proxyConfigHolder) {
+        ProxyNetworkConfig networkConfig = proxyConfigHolder.getProxyNetworkConfig();
+        ProxyCredentials proxyCredentials = proxyConfigHolder.getProxyCredentials();
+
+        if (networkConfig != null && proxyCredentials != null) {
+            configureAuth(driver, proxyCredentials.getUsername(), proxyCredentials.getPassword());
+        }
+    }
+
+    private void configureAuth(WebDriver driver, String username, String password) {
+        driver.get("chrome-extension://enhldmjbphoeibbpdhmjkchohnidgnah/options.html");
+        driver.findElement(By.id("url")).sendKeys(".*");
+        driver.findElement(By.id("username")).sendKeys(username);
+        driver.findElement(By.id("password")).sendKeys(password);
+        driver.findElement(By.className("credential-form-submit")).click();
+    }
+
+    private void loadWebDriverConfigProperties(WebDriverConfig webDriverConfig) {
+        Properties properties = configPropertiesLoader.loadConfigProperties("config.properties");
+
+        webDriverConfig.setUserAgent(properties.getProperty("userAgent"));
+        webDriverConfig.setPageLoadTimeout(Long.parseLong(properties.getProperty("pageLoadTimeout")));
+        webDriverConfig.setImplicitlyWait(Long.parseLong(properties.getProperty("implicitlyWait")));
+    }
 }
