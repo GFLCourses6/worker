6fb40926f5b2108c3311da9bdb06e5073e6f8360
Olia-dolia <dolechka4@gmail.com>
2023-11-13 20:07:54 +0200

    Changes logic of ProxySourcesClientService class

--- executor-service/src/main/java/executor/service/service/proxy/ProxySourcesClientService.java
+++ executor-service/src/main/java/executor/service/service/proxy/ProxySourcesClientService.java
@@ -1,58 +1,60 @@
 package executor.service.service.proxy;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import executor.service.holder.ProxyCredentialsHolder;
+import executor.service.holder.ProxyNetworkConfigHolder;
 import executor.service.model.ProxyConfigHolder;
 import executor.service.model.ProxyCredentials;
 import executor.service.model.ProxyNetworkConfig;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class ProxySourcesClientService implements ProxySourcesClient {
     private static final String PROXY_CREDENTIALS_PATH = "src/json/ProxyCredentials.json";
     private static final String PROXY_NETWORK_CONFIG_PATH = "src/json/ProxyNetworkConfig.json";
     private final ObjectMapper objectMapper;
+    private final ProxyNetworkConfigHolder networkConfigHolder;
+    private final ProxyCredentialsHolder credentialsHolder;
 
-    public ProxySourcesClientService(ObjectMapper objectMapper) throws IOException {
+    public ProxySourcesClientService(ObjectMapper objectMapper, ProxyNetworkConfigHolder networkConfigHolder,
+                                      ProxyCredentialsHolder credentialsHolder) throws IOException {
         this.objectMapper = objectMapper;
+        this.networkConfigHolder = networkConfigHolder;
+        this.credentialsHolder = credentialsHolder;
+        getConfigs();
+        getCredentials();
     }
 
-    private List<ProxyCredentials> getProxyCredentialsFromFile() throws IOException {
+    private <T> List<T> getProxyInfoFromFile(String path, Class<T> type) throws IOException {
         return objectMapper.readValue(
-                new File(PROXY_CREDENTIALS_PATH),
+                new File(path),
                 objectMapper.getTypeFactory()
-                        .constructCollectionType(List.class, ProxyCredentials.class));
+                        .constructCollectionType(List.class, type));
     }
 
-    private List<ProxyNetworkConfig> getProxyNetworkConfigFromFile() throws IOException {
-        return objectMapper.readValue(
-                new File(PROXY_NETWORK_CONFIG_PATH),
-                objectMapper.getTypeFactory()
-                        .constructCollectionType(List.class, ProxyNetworkConfig.class));
+    private void getConfigs() throws IOException {
+        List<ProxyNetworkConfig> proxyNetworkConfigs = getProxyInfoFromFile(PROXY_NETWORK_CONFIG_PATH, ProxyNetworkConfig.class);
+        networkConfigHolder.addAll(proxyNetworkConfigs);
+    }
+
+    private void getCredentials() throws IOException {
+        List<ProxyCredentials> proxyCredentials = getProxyInfoFromFile(PROXY_CREDENTIALS_PATH, ProxyCredentials.class);
+        credentialsHolder.addAll(proxyCredentials);
     }
 
     @Override
-    public ProxyConfigHolder getProxy() throws IOException {
-        List<ProxyCredentials> proxyCredentials = getProxyCredentialsFromFile();
-        List<ProxyNetworkConfig> proxyNetworkConfigs = getProxyNetworkConfigFromFile();
-        List<ProxyConfigHolder> proxyConfigHolders = new ArrayList<>();
-        int difference = proxyCredentials.size() - proxyNetworkConfigs.size();
-        int j = 0;
-        for (int i = 0; i < proxyNetworkConfigs.size() + difference; i++) {
-            if (i >= proxyNetworkConfigs.size()) {
-                proxyConfigHolders.add(new ProxyConfigHolder(proxyNetworkConfigs.get(j), proxyCredentials.get(i)));
-                j++;
-                if (j == proxyNetworkConfigs.size()) j = 0;
-            } else {
-                proxyConfigHolders.add(new ProxyConfigHolder(proxyNetworkConfigs.get(i), proxyCredentials.get(i)));
-            }
+    public ProxyConfigHolder getProxy() {
+        ProxyConfigHolder proxyConfigHolder = new ProxyConfigHolder();
+        try {
+            proxyConfigHolder.setProxyNetworkConfig(networkConfigHolder.poll().get());
+            proxyConfigHolder.setProxyCredentials(credentialsHolder.poll().get());
+            return proxyConfigHolder;
+        } catch (NoSuchElementException e){
+            throw new NoSuchElementException("ProxyNetworkConfig is null");
         }
-        for (ProxyConfigHolder p : proxyConfigHolders) {
-            return p;
-        }
-        return null;
     }
 }
 
