a6a76f836d9a93f3e8f2e55feb17ab4509a47c47
Lokankara <poliyakpavlo@gmail.com>
2023-12-14 21:37:06 +0200

    Removed bean ObjectMapper

--- executor-service/src/main/java/executor/service/config/AppConfig.java
+++ executor-service/src/main/java/executor/service/config/AppConfig.java
@@ -1,6 +1,5 @@
 package executor.service.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import executor.service.facade.parallel.ConfigurableThreadPool;
 import executor.service.model.ThreadPoolConfig;
 import executor.service.model.WebDriverConfig;
@@ -13,11 +12,6 @@ import java.util.concurrent.ThreadPoolExecutor;
 @Configuration
 @Import({WebDriverConfig.class, ThreadPoolConfig.class})
 public class AppConfig {
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        return new ObjectMapper();
-    }
 
     @Bean
     public ThreadPoolExecutor threadPoolExecutor(ThreadPoolConfig threadPoolConfig) {
