72098b505045aaa6558817b669a9310030171e42
Mazurenko Nikita <nikitamazurenko42@gmail.com>
2024-02-01 15:53:14 +0200

    Assign scenarioMessage to Scenario after the failed execution

--- executor-service/src/main/java/executor/service/service/executor/Action.java
+++ executor-service/src/main/java/executor/service/service/executor/Action.java
@@ -8,5 +8,4 @@ public final class Action {
     public static final String CLICK_CSS_ACTION = "clickCss";
     public static final String CLICK_XPATH_ACTION = "clickXpath";
     public static final String SLEEP_ACTION = "sleep";
-    public static final String UNSUPPORTED_ACTION = "unsupported";
 }
